6501d83c17031952687c45980f1dc58dabffafb2
JudeUk <Judeukana@gmail.com>
2021-08-30 20:24:50 +0100

    Refactor clean up

--- app/build/generated/source/buildConfig/debug/com/payx/PayXant/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/payx/PayXant/BuildConfig.java
@@ -5,7 +5,7 @@ package com.payx.PayXant;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.bolu.myapplication4";
+  public static final String APPLICATION_ID = "com.payx.PayXant";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
